d932e52a6b3553246526b89fe36042bb3300fd33
Ezekiel Mangalu <emangalu@gmail.com>
2019-09-18 17:29:44 +0800

    Clean up and testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const { getByTestId } = render(<App />);
+  expect(getByTestId("App")).toBeTruthy();
 });
