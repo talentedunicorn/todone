06a0f61fbea8c8ed8d7c75039855cc65a0123fbf
Samantha Lau <samantha.lhe@gmail.com>
2019-10-14 15:52:14 +0800

    Create edit task logic in list

--- src/components/list.js
+++ src/components/list.js
@@ -1,39 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import "./list.css";
 
-const List = ({ items, handleItemClick, handleDelete, handleEdit }) => (
-  <ol
-    data-testid="List"
-    className="List"
-    data-empty-message="No items added..."
-  >
-    {items &&
-      items.map(item => (
-        <li key={item.id}>
-          <p
-            title={item.text}
-            onClick={() => handleItemClick(item.id)}
-            data-completed={item.completed ? true : undefined}
-          >
-            {item.text}
-          </p>
-          <button
-            onClick={() => handleEdit(item.id)}
-            className="Button-icon edit"
-          >
-            Edit
-          </button>
-          {handleDelete && (
-            <button
-              onClick={() => handleDelete(item.id)}
-              className="Button-icon delete"
-            >
-              Delete
-            </button>
-          )}
-        </li>
-      ))}
-  </ol>
-);
+const List = ({ items, handleItemClick, handleDelete, setTodos, todos }) => {
+  const [todoToEdit, setTodoToEdit] = useState(null);
+
+  const handleEdit = item => {
+    setTodoToEdit(item);
+  };
+
+  const editTodo = e => {
+    e.preventDefault();
+    const newTodos = todos.map(todo =>
+      todo.id === todoToEdit.id ? todoToEdit : todo
+    );
+    setTodos(newTodos);
+    setTodoToEdit(null);
+  };
+
+  return (
+    <ol
+      data-testid="List"
+      className="List"
+      data-empty-message="No items added..."
+    >
+      {items &&
+        items.map(item => {
+          return (
+            <li key={item.id}>
+              {todoToEdit && item.id === todoToEdit.id ? (
+                <form className="EditForm">
+                  <input
+                    type="text"
+                    value={todoToEdit.text}
+                    onChange={e =>
+                      setTodoToEdit({ ...todoToEdit, text: e.target.value })
+                    }
+                  />
+                  <button onClick={editTodo}>Save Task</button>
+                  <button
+                    className="cancel"
+                    onClick={() => setTodoToEdit(null)}
+                  >
+                    Cancel
+                  </button>
+                </form>
+              ) : (
+                <>
+                  <p
+                    title={item.text}
+                    onClick={() => handleItemClick(item.id)}
+                    data-completed={item.completed ? true : undefined}
+                  >
+                    {item.text}
+                  </p>
+                  {todos && (
+                    <button
+                      onClick={() => handleEdit(item)}
+                      className="Button-icon edit"
+                    >
+                      Edit
+                    </button>
+                  )}
+                  {handleDelete && (
+                    <button
+                      onClick={() => handleDelete(item.id)}
+                      className="Button-icon delete"
+                    >
+                      Delete
+                    </button>
+                  )}
+                </>
+              )}
+            </li>
+          );
+        })}
+    </ol>
+  );
+};
 
 export default List;
