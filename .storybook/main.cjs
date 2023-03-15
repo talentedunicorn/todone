4e291abcdc8073be602047226eae4e4cff1530c9
Talentedunicorn <dev@talentedunicorn.com>
2023-03-15 12:01:08 +0800

    Updated storybook to v7

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,29 +1,23 @@
 const path = require('path');
-
 module.exports = {
-	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions'
-	],
-	framework: '@storybook/svelte',
-	core: {
-		builder: '@storybook/builder-vite'
-	},
-	svelteOptions: {
-		preprocess: import('../svelte.config.js').preprocess
-	},
-	features: {
-		storyStoreV7: true
-	},
-	viteFinal: async (config) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			$lib: path.resolve(__dirname, '../src/lib'),
-			'$env/static/public': process.env
-		};
-
-		return config;
-	}
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/sveltekit',
+    options: {}
+  },
+  features: {
+    storyStoreV7: true
+  },
+  viteFinal: async config => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      $lib: path.resolve(__dirname, '../src/lib'),
+      '$env/static/public': process.env
+    };
+    return config;
+  },
+  docs: {
+    autodocs: true
+  }
 };
