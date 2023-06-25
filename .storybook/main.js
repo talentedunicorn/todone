b976120f64b7a2cd97288ffd52ce7f975ef3c245
Talentedunicorn <dev@talentedunicorn.com>
2023-06-25 21:24:34 +0800

    Setup testing and coverage on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,14 @@
+const istanbul = require('vite-plugin-istanbul');
+const constants = require('@storybook/addon-coverage/dist/cjs/constants');
+const { mergeConfig } = require('vite');
+
 export default {
 	stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
 		'@storybook/addon-interactions',
+		'@storybook/addon-coverage',
 		'@storybook/addon-svelte-csf'
 	],
 	framework: {
@@ -13,9 +18,23 @@ export default {
 		}
 	},
 	features: {
-		storyStoreV7: true
+		storyStoreV7: true,
+		buildStoriesJson: true
 	},
 	docs: {
 		autodocs: true
+	},
+	async viteFinal(config) {
+		return mergeConfig(config, {
+			build: {
+				sourcemap: true
+			},
+			plugins: [
+				istanbul({
+					exclude: constants.defaultExclude,
+					extension: constants.defaultExtensions
+				})
+			]
+		});
 	}
 };
