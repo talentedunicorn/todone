c2118246563e497a623ff87365ab2ea17943cdec
Talentedunicorn <dev@talentedunicorn.com>
2023-06-25 23:05:14 +0800

    Menu tests

--- src/components/Menu.stories.svelte
+++ src/components/Menu.stories.svelte
@@ -1,14 +1,51 @@
 <script lang="ts">
 	import { Meta, Story, Template } from '@storybook/addon-svelte-csf';
 	import Menu from './Menu.svelte';
+	import { userEvent, waitFor, within } from '@storybook/testing-library';
+	import { expect } from '@storybook/jest';
 </script>
 
 <Meta title="Menu" component={Menu} />
 
 <Template let:args>
-	<Menu {...args} menuItems={[{ label: 'ToDo', selected: true }, { label: 'Done' }]} />
+	<Menu
+		{...args}
+		menuItems={[{ label: 'ToDo', selected: true }, { label: 'Done' }]}
+		on:goTo={() => {}}
+	/>
 </Template>
 
-<Story name="Default" />
+<Story
+	name="Default"
+	play={({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const toggleButton = canvas.getByRole('button');
+		const menu = canvas.queryByTestId('menu');
 
-<Story name="With title" args={{ title: 'Tasks' }} />
+		expect(menu).not.toBeInTheDocument();
+		userEvent.click(toggleButton);
+		waitFor(function () {
+			expect(canvas.getByTestId('menu')).toBeInTheDocument();
+			const todoLink = canvas.getByText('ToDo');
+			const doneLink = canvas.getByText('Done');
+
+			expect(todoLink.classList).toContain('selected');
+			userEvent.click(doneLink);
+			expect(menu).not.toBeInTheDocument();
+		});
+	}}
+/>
+
+<Story
+	name="With title"
+	args={{ title: 'Tasks' }}
+	play={({ canvasElement }) => {
+		const canvas = within(canvasElement);
+		const toggleButton = canvas.getByRole('button');
+		userEvent.click(toggleButton);
+
+		waitFor(() => {
+			expect(canvas.getByTestId('header').textContent).toBe('Tasks');
+		});
+	}}
+/>
