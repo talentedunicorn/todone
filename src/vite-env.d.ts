4d1681da367cf5bfac51ca08ca4fcd3cea0e141b
Talentedunicorn <dev@talentedunicorn.com>
2024-07-01 23:29:42 +0800

    Added types to vite envs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,3 +2,18 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-pwa/svelte" />
 /// <reference types="vite-plugin-pwa/info" />
+
+interface ImportMetaEnv {
+	readonly VITE_SYNCED: string;
+	readonly VITE_GA_TAG: string;
+	readonly VITE_DB_NAME: string;
+	readonly VITE_REMOTE_DB: string;
+	readonly VITE_REMOTE_DB_USERNAME: string;
+	readonly VITE_REMOTE_DB_PASSWORD: string;
+	readonly VITE_AUTH0_CLIENT: string;
+	readonly VITE_AUTH0_DOMAIN: string;
+}
+
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
