c03844be74cfbc0ff177d408fd814cbe2f7fda38
Talentedunicorn <dev@talentedunicorn.com>
2024-07-11 19:34:36 +0800

    Added sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { VitePWA } from 'vite-plugin-pwa';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+	build: {
+		sourcemap: true
+	},
 	plugins: [
 		svelte(),
 		VitePWA({
@@ -35,6 +39,11 @@ export default defineConfig(({ mode }) => ({
 					}
 				]
 			}
+		}),
+		sentryVitePlugin({
+			authToken: process.env.SENTRY_AUTH_TOKEN,
+			org: 'talentedunicorn',
+			project: 'todone'
 		})
 	],
 	...(mode === 'development' && {
