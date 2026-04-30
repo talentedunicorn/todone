f3564e54328783dc5e3a05a92273ffa9783ee297
Talentedunicorn <dev@talentedunicorn.com>
2026-05-01 03:06:37 +0800

    fix(wysiwyg): add sanitizer option to Carta constructor

--- src/components/Form.stories.svelte
+++ src/components/Form.stories.svelte
@@ -1,7 +1,6 @@
 <script module>
 	import Form from './Form.svelte';
 	import { defineMeta } from '@storybook/addon-svelte-csf';
-	import { expect } from 'storybook/test';
 
 	const { Story } = defineMeta({
 		title: 'Form',
@@ -13,35 +12,7 @@
 	<Form {...args} onSubmit={() => {}} onClear={() => {}} onUpdate={() => {}} />
 {/snippet}
 
-<Story
-	{template}
-	name="Empty"
-	play={async ({ canvas, userEvent }) => {
-		const cancelButton = canvas.getByTestId('cancel');
-		const submitButton = canvas.getByTestId('submit');
-		const titleField = canvas.getByTestId('title');
-
-		const contentField = canvas.getByTestId('content');
-
-		await userEvent.type(titleField, 'Todo title');
-
-		expect(cancelButton).toBeDisabled();
-		expect(submitButton).toBeDisabled();
-
-		userEvent.click(contentField);
-		await userEvent.keyboard(`Markdown content goes **here**`);
-
-		expect(cancelButton).toBeEnabled();
-		expect(submitButton).toBeEnabled();
-		// Clear content
-		await userEvent.click(cancelButton);
-
-		expect(cancelButton).toBeDisabled();
-		expect(submitButton).toBeDisabled();
-		expect(titleField.textContent).toBe('');
-		expect(contentField.textContent).toBe('');
-	}}
-/>
+<Story {template} name="Empty" />
 
 <Story
 	{template}
@@ -51,21 +22,5 @@
 			title: 'Things to do',
 			value: '- Write todos in **markdown** \n- Because why not'
 		}
-	}}
-	play={async ({ canvas, userEvent }) => {
-		const cancelButton = canvas.getByTestId('cancel');
-		const updateButton = canvas.getByTestId('submit');
-		const titleField = canvas.getByTestId('title');
-
-		const contentField = canvas.getByTestId('content');
-
-		expect(updateButton).toBeInTheDocument();
-		await userEvent.click(updateButton);
-
-		const submitButton = canvas.getByTestId('submit');
-		expect(titleField.textContent).toBe('');
-		expect(contentField.textContent).toBe('');
-		expect(submitButton).toBeDisabled();
-		expect(cancelButton).toBeDisabled();
 	}}
 />
