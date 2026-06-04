7d010fa39e440cc560c47ea25e83c76a46423b63
Talentedunicorn <dev@talentedunicorn.com>
2026-06-04 16:45:50 +0800

    add FullScreenEditor story

---
+++ src/components/FullScreenEditor.stories.svelte
@@ -0,0 +1,47 @@
+<script module lang="ts">
+	import FullScreenEditor from './FullScreenEditor.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { fn } from 'storybook/test';
+
+	const { Story } = defineMeta({
+		title: 'FullScreenEditor',
+		component: FullScreenEditor,
+		args: {
+			open: true,
+			onClose: fn(),
+			onSave: fn()
+		},
+		argTypes: {
+			open: { control: 'boolean', default: true }
+		}
+	});
+</script>
+
+<Story name="New task" args={{ defaultValue: null }} />
+
+<Story
+	name="Edit task"
+	args={{
+		defaultValue: {
+			id: '1',
+			title: 'Example task',
+			value: '# Things to do\n\n- [ ] First item\n- [ ] Second item',
+			status: 'todo',
+			updated: new Date()
+		}
+	}}
+/>
+
+<Story
+	name="With delete"
+	args={{
+		defaultValue: {
+			id: '1',
+			title: 'Deletable task',
+			value: 'Some content here.',
+			status: 'done',
+			updated: new Date()
+		},
+		onDelete: fn()
+	}}
+/>
