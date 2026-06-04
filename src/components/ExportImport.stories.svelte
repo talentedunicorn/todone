1cc01975a033ba8b2b5d6e8b03a8a02fa4c27fa7
Talentedunicorn <dev@talentedunicorn.com>
2026-06-04 16:45:32 +0800

    add ExportImport story

---
+++ src/components/ExportImport.stories.svelte
@@ -0,0 +1,12 @@
+<script module lang="ts">
+	import ExportImport from './ExportImport.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'ExportImport'
+	});
+</script>
+
+<Story name="Default">
+	<ExportImport />
+</Story>
