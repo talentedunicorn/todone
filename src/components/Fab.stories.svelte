5f06476178fb2a433a2f785c44aafa2ebe8b15c6
Talentedunicorn <dev@talentedunicorn.com>
2026-06-04 16:45:16 +0800

    add Fab story

---
+++ src/components/Fab.stories.svelte
@@ -0,0 +1,20 @@
+<script module lang="ts">
+	import Fab from './Fab.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import { fn } from 'storybook/test';
+
+	const { Story } = defineMeta({
+		title: 'Fab',
+		component: Fab,
+		args: {
+			onclick: fn()
+		},
+		argTypes: {
+			visible: { control: 'boolean', default: true }
+		}
+	});
+</script>
+
+<Story name="Visible" args={{ visible: true }} />
+
+<Story name="Hidden" args={{ visible: false }} />
