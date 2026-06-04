1f9726e39e75bfff55f9080335818ef55eb576cc
Talentedunicorn <dev@talentedunicorn.com>
2026-06-04 16:44:59 +0800

    add ToggleTheme story

---
+++ src/components/ToggleTheme.stories.svelte
@@ -0,0 +1,12 @@
+<script module lang="ts">
+	import ToggleTheme from './ToggleTheme.svelte';
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+
+	const { Story } = defineMeta({
+		title: 'ToggleTheme'
+	});
+</script>
+
+<Story name="Default">
+	<ToggleTheme />
+</Story>
